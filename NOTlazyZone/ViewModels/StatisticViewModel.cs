9b5aab8af37ab32996b68795c645eff35683a4ce
Michał Kwaśniewski <apparentlynotapplications@gmail.com>
2023-05-20 12:30:10 +0200

    dodanie wszystkich properties i obsluga propiersites LiczbaCzlonkow

--- NOTlazyZone/ViewModels/StatisticViewModel.cs
+++ NOTlazyZone/ViewModels/StatisticViewModel.cs
@@ -37,25 +37,31 @@ namespace NOTlazyZone.ViewModels
                 }
             }
         }
-        public string Adres
-        {
-            get => Model.Adres;
-            set
-            {
-                if (Model.Adres != value)
-                {
-                    Model.Adres = value;
-                    OnPropertyChanged(() => Adres);
-                }
-            }
-        }
-
+        
+        public string Adres { get => Model.Adres; set{}}
+        //public string Test { get; set; }
+        //public string NazwaSilowni { get; set; }
+        //public string Adres { get; set; }
+        //public string Miasto { get; set; }
+        public int LiczbaCzlonkow { get => Model.LiczbaCzlonkow; set { } }
+        //public DateTime DataZalozenia { get; set; }
+        //public string TelefonKontaktowy { get; set; }
+        //public string EmailKontaktowy { get; set; }
+        //public string StronaInternetowa { get; set; }
+        //public string GodzinyOtwarcia { get; set; }
+        //public int LiczbaKlientow { get; set; }
+        //public int SredniaLiczbaOdwiedzinTygodniowo { get; set; }
+        //public string NajpopularniejszeZajecia { get; set; }
+        //public bool DostepnePosilkiDietetyczne { get; set; }
+        //public double ProcentowyWzrostLiczbyKlientow { get; set; }
+        //public string OpisWzrostu { get; set; }
 
         public Statistic Model { get; set; }
+
         public StatisticViewModel() 
         {
             base.DisplayName = "Statystyki";
-            Model = new Statistic("NOTlazyZone", "Długa 20");
+            Model = new Statistic("NOTlazyZone", "Długa 20",13);
         }
     }
     
