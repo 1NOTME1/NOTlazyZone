d33a3ea23aee63a4e691889cde88f97126c8a825
Michał Kwaśniewski <apparentlynotapplications@gmail.com>
2023-05-21 09:11:56 +0200

    dodanie wszystkiego do konstruktora

--- NOTlazyZone/Model/Statistic.cs
+++ NOTlazyZone/Model/Statistic.cs
@@ -4,9 +4,9 @@ namespace NOTlazyZone.Model
 {
     public class Statistic
     {
-        public string Test { get; set; }
         public string NazwaSilowni { get; set; }
         public string Adres { get; set; }
+        public string Wlasciciel { get; set; }
         public string Miasto { get; set; }
         public int LiczbaCzlonkow { get; set; }
         public DateTime DataZalozenia { get; set; }
@@ -21,11 +21,27 @@ namespace NOTlazyZone.Model
         public double ProcentowyWzrostLiczbyKlientow { get; set; }
         public string OpisWzrostu { get; set; }
 
-        public Statistic(string nazwaSilowni, string adres, int liczbaCzlonkow)
+        public Statistic( string nazwaSilowni, string adres, string wlasciciel, string miasto, int liczbaCzlonkow, DateTime dataZalozenia, string telefonKontaktowy, string emailKontaktowy, string stronaInternetowa, string godzinyOtwarcia, int liczbaKlientow, int sredniaLiczbaOdwiedzinTygodniowo, string najpopularniejszeZajecia, bool dostepnePosilkiDietetyczne, double procentowyWzrostLiczbyKlientow, string opisWzrostu)
         {
             this.NazwaSilowni = nazwaSilowni;
             this.Adres = adres;
+            this.Wlasciciel = wlasciciel;
+            this.Miasto = miasto;
             this.LiczbaCzlonkow = liczbaCzlonkow;
+            this.DataZalozenia = dataZalozenia;
+            this.TelefonKontaktowy = telefonKontaktowy;
+            this.EmailKontaktowy = emailKontaktowy;
+            this.StronaInternetowa = stronaInternetowa;
+            this.GodzinyOtwarcia = godzinyOtwarcia;
+            this.LiczbaKlientow = liczbaKlientow;
+            this.SredniaLiczbaOdwiedzinTygodniowo = sredniaLiczbaOdwiedzinTygodniowo;
+            this.NajpopularniejszeZajecia = najpopularniejszeZajecia;
+            this.DostepnePosilkiDietetyczne = dostepnePosilkiDietetyczne;
+            this.ProcentowyWzrostLiczbyKlientow = procentowyWzrostLiczbyKlientow;
+            this.OpisWzrostu = opisWzrostu;
         }
+
+
+
     }
 }
