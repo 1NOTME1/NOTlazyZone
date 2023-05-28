64cef28cb28a5ffcd15fca68a9c1d6514ebbad54
Michał Kwaśniewski <apparentlynotapplications@gmail.com>
2023-05-28 12:47:05 +0200

    dodanie View i ViewModeli dla poszczególnych klas

---
+++ NOTlazyZone/ViewModels/ExerciseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using NOTlazyZone.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOTlazyZone.ViewModels
+{
+    public class ExerciseViewModel : WorkspaceViewModel
+    {
+        public ExerciseViewModel()
+        {
+            base.DisplayName = "Ćwiczenia";
+        }
+    }
+}
