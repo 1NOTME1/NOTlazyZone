7fe325c1448333705d06c6723c4d9a107b9980b0
Michał Kwaśniewski <apparentlynotapplications@gmail.com>
2023-05-28 12:47:43 +0200

    Obsluga wywołania widoków z zakładek

--- NOTlazyZone/ViewModels/MainWindowViewModel.cs
+++ NOTlazyZone/ViewModels/MainWindowViewModel.cs
@@ -53,18 +53,18 @@ namespace NOTlazyZone.ViewModels
             return new List<CommandViewModel>
             {
                 new CommandViewModel("Messages",new BaseCommand(CreateMessage)),
-                new CommandViewModel("Calendar",new BaseCommand(ShowTrainingPlan)),
+                new CommandViewModel("Calendar",new BaseCommand(ShowCalendar)),
                 new CommandViewModel("Statistics",new BaseCommand(ShowStatistic)),
-                new CommandViewModel("Zaplanowane spotkania",new BaseCommand(ShowTrainingPlan)),
-                new CommandViewModel("Scheduled meetings",new BaseCommand(ShowTrainingPlan)),
-                new CommandViewModel("Exercise",new BaseCommand(ShowTrainingPlan)),
-                new CommandViewModel("Training Plan",new BaseCommand(ShowTrainingPlan)),
-                new CommandViewModel("Diet",new BaseCommand(ShowTrainingPlan)),
-                new CommandViewModel("Calories Calculator",new BaseCommand(ShowTrainingPlan)),
-                new CommandViewModel("List Contact",new BaseCommand(ShowTrainingPlan)),
-                new CommandViewModel("Shopping",new BaseCommand(ShowTrainingPlan)),
-                new CommandViewModel("Discounts",new BaseCommand(ShowTrainingPlan)),
-                new CommandViewModel("Settings",new BaseCommand(ShowTrainingPlan)),
+                new CommandViewModel("Zaplanowane spotkania",new BaseCommand(ShowCalendar)),
+                new CommandViewModel("Scheduled meetings",new BaseCommand(ShowCalendar)),
+                new CommandViewModel("Exercise",new BaseCommand(ShowExercies)),
+                new CommandViewModel("Training Plan",new BaseCommand(ShowCalendar)),
+                new CommandViewModel("Diet",new BaseCommand(ShowCalendar)),
+                new CommandViewModel("Calories Calculator",new BaseCommand(ShowCaloriesCalculator)),
+                new CommandViewModel("List Contact",new BaseCommand(ShowCalendar)),
+                new CommandViewModel("Shopping",new BaseCommand(ShowCalendar)),
+                new CommandViewModel("Discounts",new BaseCommand(ShowCalendar)),
+                new CommandViewModel("Settings",new BaseCommand(ShowSettings)),
 
             };
         }
@@ -122,7 +122,7 @@ namespace NOTlazyZone.ViewModels
             }
             SetActiveWorkspace(workspace);
         }
-        private void ShowTrainingPlan()
+        private void ShowCalendar()
         {
             TrainingPlanViewModel workspace = Workspaces.FirstOrDefault(vm => vm is TrainingPlanViewModel) as TrainingPlanViewModel;
             if (workspace == null)
@@ -132,7 +132,40 @@ namespace NOTlazyZone.ViewModels
             }
             SetActiveWorkspace(workspace);
         }
-        
+
+        private void ShowExercies()
+        {
+            ExerciseViewModel workspace = Workspaces.FirstOrDefault(vm => vm is ExerciseViewModel) as ExerciseViewModel;
+            if (workspace == null)
+            {
+                workspace = new ExerciseViewModel();
+                Workspaces.Add(workspace);
+            }
+            SetActiveWorkspace(workspace);
+        }
+
+        private void ShowCaloriesCalculator()
+        {
+            CaloriesCalculatorViewModel workspace = Workspaces.FirstOrDefault(vm => vm is CaloriesCalculatorViewModel) as CaloriesCalculatorViewModel;
+            if (workspace == null)
+            {
+                workspace = new CaloriesCalculatorViewModel();
+                Workspaces.Add(workspace);
+            }
+            SetActiveWorkspace(workspace);
+        }
+
+        private void ShowSettings()
+        {
+            SettingViewModel workspace = Workspaces.FirstOrDefault(vm => vm is SettingViewModel) as SettingViewModel;
+            if (workspace == null)
+            {
+                workspace = new SettingViewModel();
+                Workspaces.Add(workspace);
+            }
+            SetActiveWorkspace(workspace);
+        }
+
         private void SetActiveWorkspace(WorkspaceViewModel workspace)
         {
             Debug.Assert(this.Workspaces.Contains(workspace));
