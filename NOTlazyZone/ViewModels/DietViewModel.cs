6d17740ea6d598d302891946cc2b3dc4fd7af5b8
Michał Kwaśniewski <apparentlynotapplications@gmail.com>
2023-06-04 20:19:07 +0200

    nie wiem co sie zmienilo xd

---
+++ NOTlazyZone/ViewModels/DietViewModel.cs
@@ -0,0 +1,17 @@
+﻿using NOTlazyZone.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOTlazyZone.ViewModels
+{
+    public class DietViewModel : WorkspaceViewModel
+    {
+        public DietViewModel()
+        {
+            base.DisplayName = "Dieta";
+        }
+    }
+}
