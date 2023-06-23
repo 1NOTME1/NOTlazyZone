11b3b81cda2e9ef3838f4a4b8760219a96c37871
Michał Kwaśniewski <apparentlynotapplications@gmail.com>
2023-06-23 21:48:29 +0200

    dodane przekazywanie obiektu w celu dodania ikon do buttonów dla zakładek

--- NOTlazyZone/Helpers/BaseCommand.cs
+++ NOTlazyZone/Helpers/BaseCommand.cs
@@ -7,27 +7,32 @@ using System.Windows.Input;
 
 namespace NOTlazyZone.Helpers
 {
-    public class BaseCommand: ICommand
+    public class BaseCommand : ICommand
     {
-        private readonly Action _command;
+        private readonly Action<object> _command;
         private readonly Func<bool> _canExecute;
-        public BaseCommand(Action command, Func<bool> canExecute = null)
+
+        public BaseCommand(Action<object> command, Func<bool> canExecute = null)
         {
             if (command == null)
                 throw new ArgumentNullException("command");
             _canExecute = canExecute;
             _command = command;
         }
+
         public void Execute(object parameter)
         {
-            _command();
+            _command(parameter);
         }
+
         public bool CanExecute(object parameter)
         {
             if (_canExecute == null)
                 return true;
             return _canExecute();
         }
+
         public event EventHandler CanExecuteChanged;
     }
+
 }
